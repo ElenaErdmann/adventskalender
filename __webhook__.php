c4734f5deba8b35663af2fae7dd0312c610e0647
Sak Zirai <s.zirai@live.de>
2018-11-06 14:30:29 +0100

    changes hook to branch master

--- __webhook__.php
+++ __webhook__.php
@@ -17,6 +17,7 @@ if (ipMatch($_SERVER['REMOTE_ADDR'], $gh_ips) === false) {
     die(1);
 }
 $BRANCH = $_GET['branch'];
+$BRANCH = 'master'
 if (!empty($BRANCH)) {
     $output = shell_exec("cd /var/www/advent18.journocode.com/html; git pull origin {$BRANCH};");
     echo "<pre>$output</pre>";
